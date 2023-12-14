597093ddc47b4025435035e39dea72262f62a1e9
MateusMiranda20 <mateuusmiranda20@gmail.com>
2023-12-13 22:06:33 -0300

    Fazendo o carrosel

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,21 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
 
 import Category from '../../assets/categories.png'
 import api from '../../services/api'
 import { Container, CategoryImg } from './styles'
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([])
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('categories')
+      const { data } = await api.get('categories')
 
-      console.log(response)
+      setCategories(data)
     }
     loadCategories()
   }, [])
   return (
     <Container>
       <CategoryImg src={Category} alt="logo do categoria" />
+
+      <Carousel itemsToShow={4}>{categories.map}</Carousel>
     </Container>
   )
 }
